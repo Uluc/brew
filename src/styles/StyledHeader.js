d8a299eeae91fd74cf091c5285029946cd81d1b2
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2020-12-21 11:22:54 -0600

    styling update

--- src/styles/StyledHeader.js
+++ src/styles/StyledHeader.js
@@ -8,7 +8,16 @@ export const StyledHeader = styled.h1`
     color: white;
 `;
 
+export const StyledHeaderText = styled.p`
+    font-size: 1.2rem;
+    font-family: 'Numans', sans-serif;
+    font-weight: 400;
+    text-align: center;
+    color: white;
+`;
+
 export const HeaderWrapper = styled.div`
     background-color: #823535;
     height: 10rem;
+    padding-bottom: 2rem;
 `;
