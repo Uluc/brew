23426ec22560b1ef1a6ab322df62f4ab8baa28d3
Uluc <ulucozdenvar01@gmail.com>
2020-12-23 20:08:01 -0500

    structuring in class rather tham const

--- src/components/SectionItem.js
+++ src/components/SectionItem.js
@@ -9,36 +9,67 @@ import {
   DishHeading,
   DishPrice,
   StyledButton,
-  DishHeader
+  DishHeader,
 } from "../styles/StyledSection";
+import { productData } from "../data/dataItem";
 
-const SectionItem = ({ data }) => {
-  return data.map((product, index) => {
+class SectionItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: null };
+  }
+
+  componentDidMount() {
+    const { product } = [productData];
+    this.setState({ product: product });
+  }
+
+  componentWillUnmount() {}
+
+  render() {
+    const product = this.state.product;
+    if (!product) {
+        return (
+            <div>Something went wrong. Or data is empty</div>
+        )
+    }
     return (
-      <div key={index}>
-        <SectionTitle >{product.section}</SectionTitle>
-        
-        <DishContainer>
-          {product.meals.map((spec, index) => (
-            <Row key={index}>
-              <FoodCol xs={9}>
-              <DishHeader>
-                <DishHeading> {spec.name} </DishHeading>
-                
-                </DishHeader>
-                <DishInfo> {spec.desc}</DishInfo>
-              </FoodCol>
-              <PriceCol>
-                <DishPrice> {spec.price} </DishPrice>
-                
-              </PriceCol>
-            </Row>
-          ))}
-        </DishContainer>
-        
+      <div>
+        {Object.keys(product).map((product, index) => {
+          return <SectionTitle key={index}>{product.section}</SectionTitle>;
+        })}
       </div>
     );
-  });
-};
+  }
+}
+
+// const SectionItem = ({ data }) => {
+//   return data.map((product, index) => {
+//     return (
+//       <div key={index}>
+//         <SectionTitle >{product.section}</SectionTitle>
+
+//         <DishContainer>
+//           {product.meals.map((spec, index) => (
+//             <Row key={index}>
+//               <FoodCol xs={9}>
+//               <DishHeader>
+//                 <DishHeading> {spec.name} </DishHeading>
+
+//                 </DishHeader>
+//                 <DishInfo> {spec.desc}</DishInfo>
+//               </FoodCol>
+//               <PriceCol>
+//                 <DishPrice> {spec.price} </DishPrice>
+
+//               </PriceCol>
+//             </Row>
+//           ))}
+//         </DishContainer>
+
+//       </div>
+//     );
+//   });
+// };
 
 export default SectionItem;
