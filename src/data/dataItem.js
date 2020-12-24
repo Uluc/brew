dd48e80bb44f357f15bde34d97bba0bc91af75b2
Uluc <ulucozdenvar01@gmail.com>
2020-12-24 16:55:34 -0500

    example index on data

--- src/data/dataItem.js
+++ src/data/dataItem.js
@@ -1,5 +1,6 @@
 export const productData = [
   {
+    id: 0,
     section: "Appetizers",
     meals: [
       {
@@ -23,21 +24,25 @@ export const productData = [
     ],
   },
   {
+    id: 1,
     section: "Main Entree",
     meals: [
       {
+        id: 0,
         name: "Supreme Pizza",
         desc:
           "Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
         price: "$19.99",
       },
       {
+        id: 1,
         name: "Hawaiian Paradise",
         desc:
           " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
         price: "$16.99",
       },
       {
+        id: 2,
         name: "Veggie Overload",
         desc:
           " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
@@ -46,21 +51,25 @@ export const productData = [
     ],
   },
   {
+    id: 2,
     section: "Lunch",
     meals: [
       {
+        id: 4,
         name: "Supreme Pizza",
         desc:
           "Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
         price: "$19.99",
       },
       {
+        id: 5,
         name: "Hawaiian Paradise",
         desc:
           " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
         price: "$16.99",
       },
       {
+        id: 6,
         name: "Veggie Overload",
         desc:
           " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
@@ -69,21 +78,25 @@ export const productData = [
     ],
   },
   {
+    id: 4,
     section: "Salad",
     meals: [
       {
+        id: 7,
         name: "Supreme Pizza",
         desc:
           "Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
         price: "$19.99",
       },
       {
+        id: 8,
         name: "Hawaiian Paradise",
         desc:
           " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
         price: "$16.99",
       },
       {
+        id: 9,
         name: "Veggie Overload",
         desc:
           " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
