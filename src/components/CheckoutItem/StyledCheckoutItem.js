264886df50ddd07edcfa95fa21aa33a4358ff923
Uluc <ulucozdenvar01@gmail.com>
2020-12-26 10:59:47 -0500

    checkout items are fixed

--- src/components/CheckoutItem/StyledCheckoutItem.js
+++ src/components/CheckoutItem/StyledCheckoutItem.js
@@ -1,66 +1,112 @@
-import styled from 'styled-components';
-import Container from 'react-bootstrap/Container';
-import {BsPlus, BsDash, BsX} from 'react-icons/bs'
+import styled from "styled-components";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { BsPlus, BsDash, BsX } from "react-icons/bs";
 
-export const CheckoutItemContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    border-style: solid;
-    padding: 1rem;
-    margin-left: 1rem;
-    margin-right: 1rem;
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-    background-color: lightgray;
+export const CheckoutHeaderContainer = styled(Row)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
 `;
 
-export const CounterContainer = styled.div`
+export const HeaderTitle = styled.h1`
+    font-size: 1rem;
+    font-weight: 640;
+`;
+
+export const CheckoutItemContainer = styled(Row)`
+  display: flex;
+  flex-direction: row;
+  border-style: solid;
+  padding: 0;
+  border-width: 0;
+  border-bottom-width: 1px;
+  justify-content: space-between;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+export const DishCol = styled(Col)`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CounterCol = styled(Col)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PriceCol = styled(Col)`
     display: flex;
-    flex-direction: column;
+    justify-content: center;
     align-items: center;
-    border-style: solid;
-    border-radius: 0.4rem;
-    border-width: 1px;
-    border-color: #C0C0C0;
 `;
 
 export const Plus = styled(BsPlus)`
-    font-size: 1.3rem;
-    
+  font-size: 1.3rem;
+  color: gray;
 `;
 
 export const Minus = styled(BsDash)`
-    font-size: 1.3rem;
-    
-`;
-
-export const DishContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-left: 1rem;
-    margin-bottom: 0.5rem;
-    
+  font-size: 1.3rem;
+  color: gray;
 `;
 
 export const Count = styled.p`
-    margin:0;
+  margin: 0;
+  margin-left: 0.7rem;
+  margin-right: 0.7rem;
+  color: gray;
 `;
 
-export const Dish = styled.h1``;
-
-export const DishPrice = styled.p``;
-
-export const RemoveContainer = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    color: #000;
-    background-color: salmon;
-
+export const CounterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 1.4rem;
 `;
+
+export const DishContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0.5rem;
+`;
+
+export const Dish = styled.h2`
+  margin: 0;
+  font-size: 1rem;
+`;
+
+export const DishPrice = styled.p`
+    margin: 0;
+`;
+
+export const RemoveCol = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  color: #000;
+`;
+
+
 export const Remove = styled(BsX)`
-    font-size: 1.2rem;
-    justify-self: flex-end;
-    background-color: lightred;
+  font-size: 1.2rem;
+  justify-self: flex-end;
+  cursor: pointer;
+`;
+
+export const RemoveBlank = styled(BsX)`
+  font-size: 1.2rem;
+  justify-self: flex-end;
+  color: transparent;
 
 `;
+
 
--- src/components/CheckoutItem/index.js
+++ src/components/CheckoutItem/index.js
@@ -1,7 +1,8 @@
 import { render } from "@testing-library/react";
 import React, { useState } from "react";
 import {
-CheckoutItemContainer,
+  CheckoutItemContainer,
+  CheckoutHeaderContainer,
   CounterContainer,
   Plus,
   Minus,
@@ -10,33 +11,81 @@ CheckoutItemContainer,
   Dish,
   DishPrice,
   Remove,
-  RemoveContainer
+  RemoveCol,
+  DishCol,
+  CounterCol,
+  PriceCol,
+  HeaderTitle,
+  RemoveBlank,
 } from "./StyledCheckoutItem";
 const CheckoutItem = () => {
   const [count, setcount] = useState(0);
 
+  const increment = () => {
+    setcount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count != 0) {
+      setcount(count - 1);
+    }
+  };
+
   return (
-    <CheckoutItemContainer>
-      <CounterContainer>
-          <Plus/>
-          <Count> 0 </Count>
-          <Minus/>
-      </CounterContainer>
+    <div>
+      <Header />
+      <CheckoutItemContainer>
+        <DishCol xs={6}>
+          <DishContainer>
+            <Dish>Himalayan Pizza</Dish>
+            <DishPrice>19.99$</DishPrice>
+          </DishContainer>
+        </DishCol>
 
-      <DishContainer>
-        <Dish>
-            Pizza
-        </Dish>
-        <DishPrice>
-            19.99$
-        </DishPrice>
-      </DishContainer>
+        <CounterCol xs={2}>
+          <Counter
+            initCount={count}
+            increment={increment}
+            decrement={decrement}
+          />
+        </CounterCol>
+        <PriceCol xs={3}>
+          <DishPrice>19.99$</DishPrice>
+        </PriceCol>
+        <RemoveCol xs={1}>
+          <Remove />
+        </RemoveCol>
+      </CheckoutItemContainer>
+    </div>
+  );
+};
 
-      <RemoveContainer>
-        <Remove/>
-      </RemoveContainer>
+const Counter = ({ initCount, increment, decrement }) => {
+  return (
+    <CounterContainer>
+      <Minus onClick={decrement} />
+      <Count> {initCount} </Count>
+      <Plus onClick={increment} />
+    </CounterContainer>
+  );
+};
 
-    </CheckoutItemContainer>
+const Header = () => {
+  return (
+    <CheckoutHeaderContainer>
+      <DishCol xs={6}>
+        <HeaderTitle>Dish</HeaderTitle>
+      </DishCol>
+      <CounterCol xs={2}>
+        <HeaderTitle>Qty</HeaderTitle>
+      </CounterCol>
+      <PriceCol xs={3}>
+        <HeaderTitle>Price</HeaderTitle>
+      </PriceCol>
+      <RemoveCol xs={1}>
+        <RemoveBlank />
+      </RemoveCol>
+    </CheckoutHeaderContainer>
   );
 };
 
